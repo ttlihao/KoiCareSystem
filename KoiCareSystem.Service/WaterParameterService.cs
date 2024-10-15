2426458642e61e81e3d8192d4acdee7e67684775
TranVanHieu1 <123tranhieu123@gmail.com>
2024-10-15 22:42:13 +0700

    CRUD WaterParameter

---
+++ KoiCareSystem.Service/WaterParameterService.cs
@@ -0,0 +1,45 @@
+﻿using KoiCareSystem.BussinessObject.Models;
+using KoiCareSystem.Repository;
+using KoiCareSystem.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiCareSystem.Service
+{
+    public class WaterParameterService : IWaterParameterService
+    {
+        private WaterParameterRepos waterParameterRepos;
+
+        public WaterParameterService()
+        {
+            waterParameterRepos = new WaterParameterRepos();
+        }
+        public bool AddWaterParameter(WaterParameter waterParameter)
+        {
+            return waterParameterRepos.AddWaterParameter(waterParameter);
+        }
+
+        public bool DeleteWaterParameter(WaterParameter waterParameter)
+        {
+            return waterParameterRepos.DeleteWaterParameter(waterParameter);
+        }
+
+        public List<WaterParameter> GetListWaterParameters()
+        {
+            WaterParameter>waterParameter = waterParameterRepos.GetWaterParameterByID(0);
+        }
+
+        public WaterParameter GetWaterParameterByID(int PondID)
+        {
+            return waterParameterRepos.GetWaterParameterByID(PondID);
+        }
+
+        public bool UpdateWaterParameter(WaterParameter waterParameter)
+        {
+            return waterParameterRepos.UpdateWaterParameter(waterParameter);
+        }
+    }
+}
